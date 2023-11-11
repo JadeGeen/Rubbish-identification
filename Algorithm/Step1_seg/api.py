f89b15f9af34cce1fe50f29b5fdd54e8fda30b49
宗杰 <718795725@qq.com>
2023-11-11 21:58:21 +0800

    step_seg api

--- Algorithm/Step1_seg/api.py
+++ Algorithm/Step1_seg/api.py
@@ -1,5 +1,19 @@
+import numpy as np
+from utils.clip import clip_pic
+from utils.sam import SamModel, sam_checkpoint, model_type, device
 
-
-
-def api():
-    None
+def api(image:np.ndarray , n: int, overlap: float):
+    pic_list,bias_list = clip_pic(image, n, overlap)
+    model = SamModel(model_type, sam_checkpoint, device)
+    masks_list = []
+    for pic in pic_list:
+        masks_list.append(model.generate_mask(pic))
+    bboxs_list = []
+    for i in range(len(masks_list)):
+        for mask in masks_list[i]:
+            tmp = mask['bbox']
+            tmp[0] += bias_list[i][0]
+            tmp[1] += bias_list[i][1]
+            bboxs_list.append(tmp)
+    return bboxs_list
+    # None
