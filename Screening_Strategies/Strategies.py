d85ac732aaaa631a0e6f3365d0ba426ec1fa738c
宗杰 <718795725@qq.com>
2023-11-12 22:40:46 +0800

    暂未筛选，只添加数据结构

--- Screening_Strategies/Strategies.py
+++ Screening_Strategies/Strategies.py
@@ -1,3 +1,14 @@
+from match import img_similarity
+
+class my_struct:
+    def __init__(self, camera_id, time_msg, label_value, bboxs_list, pic_array):
+        self.camera_id = camera_id
+        self.time = time_msg
+        self.label = label_value
+        self.bboxs_list = bboxs_list
+        self.pic_array = pic_array
+
+
+def screening_startegies(pic : my_struct, ctex:list)->int:
 # TODO：筛选策略，具体参数和返回值待定
-def screening_startegies(pic_list):
-    return 0
+    return pic.label
