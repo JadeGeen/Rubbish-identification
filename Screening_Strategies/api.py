05b090fde7e6baf9202f3697d6291af6021024bf
Fish-sources <718795725@qq.com>
2023-11-18 19:46:51 +0800

    Recreate database and add IOU match

--- Screening_Strategies/api.py
+++ Screening_Strategies/api.py
@@ -3,23 +3,29 @@ from Strategies import screening_startegies, my_struct
 import numpy as np
 
 
-def api(camera_id:int, time_msg:str, label, bboxs_list : list, pic:np.ndarray)->None:
-    data = my_struct(camera_id=camera_id, time_msg=time_msg, label_value=label, bboxs_list= bboxs_list, pic_array=pic)
-    table_check()
-    # 获取相关摄像头上下文(暂未定时间范围，全取)
+def api(camera_id:int, time_msg:str, label, bboxs_list : list, pic:np.ndarray)->my_struct:
+    """
+    :param camera_id: 摄像头编号
+    :param time: 时间信息
+    :bboxs_list: 框信息(包含x坐标,y坐标,长,宽以及label)
+    :pic: 图片转换的numpy数组
+    :return: 自定义数据结构
+    """
+    data = my_struct(camera_id=camera_id, time_msg=time_msg, bboxs_list= bboxs_list, pic_array=pic)
+    # 获取相关摄像头的上一条存储信息
     ctext = data_load(data.camera_id)
     # 筛选获取新label
-    new_label = screening_startegies(data, ctext)
-    data.label = new_label
+    screening_startegies(data, ctext)
     # 存储筛选后的数据
     data_save(data)
+    return data
 
 # 用于用户查询数据库
 def api_search(camera_id:int, time_msg:str)->my_struct:
     """
     :param camera_id: 摄像头编号
     :param time: 时间信息
-    :return: 是否清理干净
+    :return: 框信息，根据后续要求更改
     """
     result = data_load(camera_id=camera_id, time=time_msg)
-    return result.label
+    return result.bboxs_list
