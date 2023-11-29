95ccc11778f2f76b39d15d54c455792debf2c8f0
Rhx11111 <139940572+Rhx11111@users.noreply.github.com>
2023-11-29 12:56:44 +0800

    main、README
    更新了简单算法服务器

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, request
-
+import flask
+import base64
+import logging
+import traceback
 import numpy as np
 import json
 import requests
+from flask import Flask, request
 from concurrent.futures import ProcessPoolExecutor
-
 
 from api import api
 
@@ -12,8 +14,10 @@ app = Flask(__name__)
 base = None
 target = None
 contra = False
-
 pool = ProcessPoolExecutor(2)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
 
 def Judge(target, base = None):
     if base == None and type(target) == np.ndarray:
@@ -22,29 +26,40 @@ def Judge(target, base = None):
         return 'Upload successfully'
     return 'Upload failed'
 
-
 @app.route('/')
 def hello():
     return 'This is algorithm server'
 
 @app.route('/upload', methods = ['POST'])
 def upload():
-    data = json.loads(str(request.data, 'utf-8'))
-    contra = data[contra]
-    if contra:
-        base = data[base]
-        target = data[target]
-    else:
-        target = data[target]
-    str = pool.submit(Judge, args = (target, base) if contra else (target))
-    return str
+    res = {"target" : [], "res" : {}}
+    if flask.request.method == "POST":
 
-data = pool.submit(api, (target, contra, base) if contra else (target, contra))
+        try:
+            data = flask.request.json
 
-@app.route('/download', metheds = ['GET'])
-def download():
-    res = json.dumps(data)
-    return res
+            contra = data["contra"]
+            contra = base64.b64decode(contra.encode('ascii'))
+            if contra:
+                base = data["base"]
+                base = base64.b64decode(base.encode('ascii'))
+                target = data["target"]
+                target = base64.b64decode(target.encode('ascii'))
+                info = api(target, contra, base)
+            else:
+                target = data["target"]
+                target = base64.b64decode(target.encode('ascii'))
+                info = api(target, contra)
+            res['target'] = target
+            info = base64.b64encode(bytes).decode()
+            res['res'] = info
+            
+        except Exception as e:
+            logger.warning(f'{e}')
+            logger.warning(traceback.print_exc())
+
+    return flask.jsonify(res)
 
 if __name__ == '__main__':
+    logger.info(f"server is running")
     app.run()
